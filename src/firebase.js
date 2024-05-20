3110226ffe6e9164853b64d662317826389e5fc3
Vivek Bhawsar <93441216+VivekBhawsar18@users.noreply.github.com>
2024-05-20 22:25:29 +0530

    Initial project theme setup

---
+++ src/firebase.js
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCjZIu2m6MJomlPliCdNyhw4ajz_cnubyw",
+  authDomain: "ecommerceapp-d73f0.firebaseapp.com",
+  projectId: "ecommerceapp-d73f0",
+  storageBucket: "ecommerceapp-d73f0.appspot.com",
+  messagingSenderId: "339002428779",
+  appId: "1:339002428779:web:febd1cc4c580209091102e"
+};
+
+// Initialize Firebase 
+export const app = initializeApp(firebaseConfig);
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "",
+//   authDomain: "",
+//   projectId: "",
+//   storageBucket: "",
+//   messagingSenderId: "",
+//   appId: ""
+// };
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
